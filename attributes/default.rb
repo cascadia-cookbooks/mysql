b65dd5438d3b3b15729489ff18e85d3935c0df39
Colby Olson <me@colbyolson.com>
2016-11-07 17:25:53 -0800

    remove trailing slashes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['mysql']['change_root']   = true
 default['mysql']['databases']     = { }
 default['mysql']['users']         = { }
-default['mysql']['log_dir']       = '/var/log/mysql/'
+default['mysql']['log_dir']       = '/var/log/mysql'
 default['mysql']['root_password'] = 'hMw8oVg3nz2j0TBjy6Z1/Q=='
 
 # https://tools.percona.com/wizard for base config generation
@@ -51,7 +51,7 @@ case node['platform_family']
 when 'debian'
     default['mysql']['service']      = 'mysql'
     default['mysql']['conf_file']    = '/etc/mysql/my.cnf'
-    default['mysql']['conf_import']  = '/etc/mysql/conf.d/'
+    default['mysql']['conf_import']  = '/etc/mysql/conf.d'
     default['mysql']['dependencies'] = %w(autoconf binutils-doc bison build-essential flex gettext ncurses-dev libmysqlclient-dev)
 
     case node['platform_version']
@@ -65,7 +65,7 @@ when 'debian'
 when 'fedora', 'rhel', 'centos'
     default['mysql']['service']      = 'mysqld'
     default['mysql']['conf_file']    = '/etc/my.cnf'
-    default['mysql']['conf_import']  = '/etc/my.cnf.d/'
+    default['mysql']['conf_import']  = '/etc/my.cnf.d'
     default['mysql']['dependencies'] = %w(autoconf bison flex gcc gcc-c++ gettext kernel-devel make m4 ncurses-devel patch mysql-community-devel)
     default['mysql']['dependencies'] = %w(gcc44 gcc44-c++) if node['platform_version'].to_i < 6
 
