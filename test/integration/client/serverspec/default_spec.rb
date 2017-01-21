fbd2504789feeb0e9701f1a09a4cad857062365a
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-01-20 16:23:12 -0800

    Update tests for new conf.d include

--- test/integration/client/serverspec/default_spec.rb
+++ test/integration/client/serverspec/default_spec.rb
@@ -2,18 +2,29 @@ require 'spec_helper'
 
 case os[:family]
 when 'ubuntu', 'debian'
-    config  = '/etc/mysql/my.cnf'
-    package = 'mysql-community-client'
-    mode    = 777
+    master_config  = '/etc/mysql/my.cnf'
+    master_mode    = 777
+    config         = '/etc/mysql/conf.d/client.cnf'
+    mode           = 644
+    package        = 'mysql-community-client'
 when 'redhat'
-    package = 'mysql-community-client'
-    config  = '/etc/my.cnf'
-    mode    = 644
+    master_config  = '/etc/my.cnf'
+    master_mode    = 644
+    config         = '/etc/conf.d/client.cnf'
+    mode           = 644
+    package        = 'mysql-community-client'
 end
 
 describe 'cop_mysql::install_client' do
   describe package(package) do
     it { should be_installed }
+  end
+
+  describe file(master_config) do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode master_mode }
   end
 
   describe file(config) do
