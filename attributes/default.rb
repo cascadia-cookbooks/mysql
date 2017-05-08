2059a245d37cd2c5f7e5a7639c3f41eb307985c1
Colby Olson <me@colbyolson.com>
2017-05-08 11:36:28 -0700

    add more lib deps

--- attributes/default.rb
+++ attributes/default.rb
@@ -53,7 +53,7 @@ when 'debian'
     default['mysql']['service']      = 'mysql'
     default['mysql']['conf_file']    = '/etc/mysql/my.cnf'
     default['mysql']['conf_import']  = '/etc/mysql/conf.d'
-    default['mysql']['dependencies'] = %w(autoconf binutils-doc bison build-essential flex gettext ncurses-dev libmysqlclient-dev)
+    default['mysql']['dependencies'] = %w(autoconf binutils-doc bison build-essential flex gettext ncurses-dev libmysqlclient-dev libmysqlclient20)
 
     default['mysql']['client']['packages'] = %w(mysql-community-client)
     default['mysql']['server']['packages'] = %w(mysql-community-server)
@@ -63,7 +63,7 @@ when 'rhel'
     default['mysql']['conf_file']    = '/etc/my.cnf'
     default['mysql']['conf_import']  = '/etc/my.cnf.d'
 
-    default['mysql']['dependencies'] = %w(autoconf bison flex gcc gcc-c++ gettext kernel-devel make m4 ncurses-devel patch mysql-community-devel)
+    default['mysql']['dependencies'] = %w(autoconf bison flex gcc gcc-c++ gettext kernel-devel make m4 ncurses-devel patch mysql-community-devel mysql-community-libs-compat)
     default['mysql']['dependencies'] += %w(gcc44 gcc44-c++) if node['platform_version'].to_i < 6
 
     default['mysql']['client']['packages'] = %w(mysql-community-client)
